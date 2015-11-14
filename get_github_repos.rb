bc137244f607d024416ba774ce981298d87c82d7
frostblooded <frostblooded@yahoo.com>
2015-11-14 09:30:43 +0200

    cleaned up code a bit more

--- get_github_repos.rb
+++ get_github_repos.rb
@@ -2,11 +2,14 @@ require_relative 'src/github_cloner.rb'
 require_relative 'src/file_processor.rb'
 require_relative 'src/file_writer.rb'
 
+DATA_DIR = 'data'
+REPOS_AT_A_TIME = 1
+RESULTS_FILE = 'results.json'
 
 loop do
-  GitHubCloner.download_repos 'data', 1
+  GitHubCloner.download_repos DATA_DIR, REPOS_AT_A_TIME
   puts
-  data = FileProcessor.process_files 'data'
+  data = FileProcessor.process_files DATA_DIR
   puts
-  FileWriter.write_to_file 'results.json', data
+  FileWriter.write_to_file RESULTS_FILE, data
 end
